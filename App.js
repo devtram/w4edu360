ac2500283c5b23d7be813f7212756f6b4d9b7681
umershafiq <umershafiq7@gmail.com>
2019-05-12 16:09:13 +0500

    Youtube Replica Complete

--- App.js
+++ App.js
@@ -1,350 +1,186 @@
-import React from "react";
-import { View, Text, Button, Alert,Image } from "react-native";
-import {createAppContainer ,createBottomTabNavigator, DrawerActions, TabBarBottom, createStackNavigator, TabNavigator} from "react-navigation";
-import { TextInput } from "react-native-gesture-handler";
-import Drawers from './src/Drawer';
+import React, { Component } from 'react'
+import {
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+import YouTube from 'react-native-youtube'
+import YouTubeVideo from './YouTubeVideo'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import Icons from 'react-native-vector-icons/MaterialCommunityIcons'
 
+const apiKey = 'AIzaSyBYRUPHuu9zx0HX8BZPyIfHWmSKZZYbziQ'
+const channelId = 'UC79Gv3mYp6zKiSwYemEik9A'
+const results = 30
 
-class LogoTitle extends React.Component {
-  render() {
-    return (
-      <Image
-        source={require('./src/img/profile.jpg')}
-        style={{ width: 30, height: 30, marginRight:10,justifyContent:'flex-end'}}
-      />
-    );
-  }
-}
-
-
-class HomeScreen extends React.Component {
+class App extends Component {
   static navigationOptions = {
-    title: 'MUHAMMAD TAYYAB RANA',
-    headerRight: <LogoTitle/>
-  };
+    headerStyle: {
+      backgroundColor: '#fff'
+    },
+    headerLeft: (
+      <TouchableOpacity>
+        <Image
+          style={{ height: 22, width: 98, color: '#fff', marginLeft: 25 }}
+          source={require('./images/logo.png')} />
+      </TouchableOpacity>
+    ),
+    headerRight: (
+      <View style={{ flexDirection: 'row', marginRight: 20 }}>
+        <TouchableOpacity style={{ paddingHorizontal: 5 }}>
+          <Icon name='cast-connected' size={25} color={'#555'} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{ paddingHorizontal: 5 }}>
+          <Icon name='videocam' size={25} color={'#555'} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{ paddingHorizontal: 5 }}>
+          <Icon name='search' size={25} color={'#555'} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{ paddingHorizontal: 5 }}>
+          <Icon name='account-circle' size={25} color={'#555'} />
+        </TouchableOpacity>
+      </View>
+    )
+  }
 
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`https://www.googleapis.com/youtube/v3/search/?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${results}`)
+      //fetch('https://www.googleapis.com/youtube/v3/search/?key=AIzaSyBJ3ntReiv0L19H2RoYW62LpRdIuyPhIpw&channelId=UCQzdMyuz0Lf4zo4uGcEujFw&part=snippet,id&order=date&maxResults=30')
+      .then(res => res.json())
+      .then(res => {
+        const videoId = []
+        res.items.forEach(item => {
+          videoId.push(item)
+        })
+        this.setState({
+          data: videoId
+        })
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
 
   render() {
+    const { navigate } = this.props.navigation
     return (
-      <Drawers/>
-      
-  
-     
-    );
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.body}>
+            {this.state.data.map((item, i) =>
+              <TouchableHighlight
+                key={item.id.videoId}
+                onPress={() => navigate('YouTubeVideo', { youtubeId: item.id.videoId })}>
+                {/* onPress={() => this.props.navigation.navigate('YoutubeVideo', {youtubeId: item.id.videoId})}> */}
+                <View style={styles.vids}>
+                  <Image
+                    source={{ uri: item.snippet.thumbnails.medium.url }}
+                    style={{ width: 320, height: 180 }} />
+                  <View style={styles.vidItems}>
+                    <Image
+                      source={require('./images/NightKing.jpg')}
+                      style={{ width: 40, height: 40, borderRadius: 20, marginRight: 5 }} />
+                    <Text style={styles.vidText}>{item.snippet.title}</Text>
+                    <Icon name='more-vert' size={20} color='#555' />
+                  </View>
+                </View>
+              </TouchableHighlight>
+            )}
+          </View>
+        </ScrollView>
+        <View style={styles.tabBar}>
+          <TouchableOpacity style={styles.tabItems}>
+            <Icon name='home' size={25} color='#444' />
+            <Text style={styles.tabTitle}>Home</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tabItems}>
+            <Icon name='whatshot' size={25} color='#444' />
+            <Text style={styles.tabTitle}>Trending</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tabItems}>
+            <Icon name='subscriptions' size={25} color='#444' />
+            <Text style={styles.tabTitle}>Subscriptions</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tabItems}>
+            <Icons name='bell' size={25} color='#444' />
+            <Text style={styles.tabTitle}>Activity</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tabItems}>
+            <Icon name='folder' size={25} color='#444' />
+            <Text style={styles.tabTitle}>Library</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
   }
 }
 
-
-const MainScreenNavigator = createStackNavigator({
-  Recent: { screen: HomeScreen },
-  All: { screen: HomeScreen },
-});
-
-const SimpleApp = createBottomTabNavigator({
-  Home: { screen: HomeScreen },
-  Chat: { screen: HomeScreen },
-});
-
-
-const ResumeNavigator = createStackNavigator({
-  Home: HomeScreen
-},
-  {
-    defaultNavigationOptions: {
-      title: 'Home',
-      //headerTitle: <LogoTitle />, 
-      headerTintColor: '#fff',
-      headerStyle: {
-        backgroundColor: '#000',
-      },
-    },
-    navigationOptions: {
-      tabBarLabel: 'Hossame',
-    },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  body: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    padding: 30
+  },
+  vids: {
+    paddingBottom: 30,
+    width: 320,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    borderBottomWidth: 0.6,
+    borderColor: '#aaa'
+  },
+  vidItems: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    padding: 10
+  },
+  vidText: {
+    padding: 20,
+    color: '#000'
+  },
+  tabBar: {
+    backgroundColor: '#fff',
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    borderTopWidth: 0.5,
+    borderColor: '#bbb'
+  },
+  tabItems: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 5
+  },
+  tabTitle: {
+    fontSize: 11,
+    color: '#333',
+    paddingTop: 4,
+    textDecorationLine: 'underline'
   }
-);
+})
 
+const screens = createStackNavigator({
+  Home: { screen: App },
+  YouTubeVideo: { screen: YouTubeVideo }
+})
 
-
-// const Home = createStackNavigator({
-//   Home: { screen: HomeScreen },
-//   Education: { screen: EducationScreen },
-//   Home: HomeScreen,
-//   Education: EducationScreen,
-//   Personal: BioDetailsScreen,
-//   Experience: ExperienceScreen
-//   },
-  
-//   {
-
-    
-//   defaultNavigationOptions: {
-//     title: 'Home',
-//     headerRight: <LogoTitle />,
-//     headerTintColor: '#fff',
-//     headerStyle: {
-//       backgroundColor: '#000',
-//     },
-//   },
-//   navigationOptions: {
-//     tabBarLabel: 'Main CV Page',
-//     headerTintColor: 'black',
-//     headerStyle: {
-//       fontSize: 33,
-//       backgroundColor: '#ff0',
-//       fontWeight:"bold",
-//     },
-
-//   },
-// }
-// );
-
-
-
-
-
-//onst Tabs = createBottomTabNavigator({ bottomMenu });
-
-//OLD APPCONTAINER OF LAST NIGHT 
-//export default createAppContainer(ResumeNavigator);  
-/* 
-  export default createAppContainer(createBottomTabNavigator(
-    {
-      Home: { screen: HomeScreen },
-    
-    },
-
-    {
-      navigationOptions: ({ navigation }) => ({
-        tabBarLabel: 'Home',
-        tabBarIcon: ({ focused, tintColor }) => {
-          const { routeName } = navigation.state;
-          let iconName;
-          if (routeName === 'Home') {
-            iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-          } else if (routeName === 'Settings') {
-            iconName = `ios-options${focused ? '' : '-outline'}`;
-          }
-
-          // You can return any component that you like here! We usually use an
-          // icon component from react-native-vector-icons
-          return <Ionicons name={iconName} size={25} color={tintColor} />;
-        },
-      }),
-      tabBarComponent: TabBarBottom,
-      tabBarPosition: 'bottom',
-      tabBarOptions: {
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
-      },
-      animationEnabled: false,
-      swipeEnabled: false,
-    }
-  ));
-
- */
-
-export default createAppContainer(ResumeNavigator);  
-
-
-
-// import React, { Component } from 'react';
-
-// import {
-//   AppRegistry,
-//   Text,
-//   TouchableOpacity,
-//   View,
-//   StyleSheet,
-//   Image,
-//   Button,
-//   TouchableNativeFeedback,
-//   Platform,
-//   Dimensions
-// } from 'react-native';
-
-// import { createStackNavigator, createBottomTabNavigator, TabView, createAppContainer } from 'react-navigation';
-// import Icon from 'react-native-vector-icons/FontAwesome';
-
-
-// console.disableYellowBox = true;
-
-// class HomeScreen extends Component {
-//   static navigationOptions = {
-//     tabBarLabel: 'Home',
-//     title: 'Home 01',
-//     tabBarIcon: ({ tintColor, focused }) => (
-//       <View>
-//         <Icon name="home" size={24} color={tintColor} />
-//       </View>
-//     ),
-//   };
-
-//   render() {
-//     return (
-//      <View></View>
-//     );
-//   }
-// }
-
-// class HomeScreen2 extends Component {
-//   static navigationOptions = {
-//     tabBarLabel: 'Home',
-//     title: 'Home 02',
-//     tabBarIcon: ({ tintColor, focused }) => (
-//       <View>
-//         <Icon name="home" size={24} color={tintColor} />
-//       </View>
-//     ),
-//   };
-
-//   render() {
-//     const {state} = this.props.navigation;
-//     return (
-//       <View>
-//         <Text>{`Name: ${state.params.name}`}</Text>
-//         <Text>{`Age: ${state.params.age}`}</Text>
-//         <Button
-//           onPress={() => this.props.navigation.goBack()}
-//           title="Go Home 01"
-//         />
-//       </View>
-//     );
-//   }
-// }
-
-// class NotificationsScreen extends Component {
-//   static navigationOptions = {
-//     tabBarLabel: 'Notifications',
-//     title: 'Notifications 01',
-//     tabBarIcon: ({ tintColor }) => (
-//       <View>
-//         <Icon name="bell" size={24} color={tintColor} />
-//       </View>
-//     ),
-//   };
-
-//   render() {
-//     return (
-//       <Button
-//         onPress={() => this.props.navigation.navigate('NotificationsTwo')}
-//         title="Go Notifications 02"
-//       />
-//     );
-//   }
-// }
-
-// class NotificationsScreen2 extends Component {
-//   static navigationOptions = {
-//     tabBarLabel: 'Notifications',
-//     title: 'Notifications 02',
-//     tabBarIcon: ({ tintColor }) => (
-//       <View>
-//         <Icon name="bell" size={24} color={tintColor} />
-//       </View>
-//     ),
-//   };
-
-//   render() {
-//     return (
-//       <Button
-//         onPress={() => this.props.navigation.goBack()}
-//         title="Go Notifications 01"
-//       />
-//     );
-//   }
-// }
-
-// class ProfileScreen extends Component {
-//   static navigationOptions = {
-//     tabBarLabel: 'Profile',
-//     title: 'Profile 01',
-//     tabBarIcon: ({ tintColor }) => (
-//       <View>
-//         <Icon name="user" size={24} color={tintColor} />
-//       </View>
-//     ),
-//   };
-
-//   render() {
-//     return (
-//       <Button
-//         onPress={() => this.props.navigation.navigate('ProfileTwo')}
-//         title="Go Profile 02"
-//       />
-//     );
-//   }
-// }
-
-// class ProfileScreen2 extends Component {
-//   static navigationOptions = {
-//     tabBarLabel: 'Profile',
-//     title: 'Profile 02',
-//     tabBarIcon: ({ tintColor }) => (
-//       <View>
-//         <Icon name="user" size={24} color={tintColor} />
-//       </View>
-//     ),
-//   };
-
-//   render() {
-//     return (
-//       <Button
-//         onPress={() => this.props.navigation.goBack()}
-//         title="Profile 02"
-//       />
-//     );
-//   }
-// }
-
-// export const Home = createStackNavigator(
-//   {
-//     HomeOne: { screen: HomeScreen},
-//     HomeTwo: { screen: HomeScreen2},
-//   },
-//   {
-//     cardStyle: {
-//       shadowOpacity: 0,
-//       backgroundColor: 'black',
-//     }
-//   }
-// );
-
-// // 
-// export const Profile = createStackNavigator(
-//   {
-//     ProfileOne: { screen: HomeScreen},
-//     ProfileTwo: { screen: ProfileScreen2},
-//   },
-//   {
-//     cardStyle: {
-//       shadowOpacity: 0,
-//       backgroundColor: 'brown',
-//     }
-//   }
-// );
-
-// const SimpleApp = createBottomTabNavigator( 
-//   {
-//      Home: Home, // a StackNavigator object
-//      Profile: Profile,
-//   }, 
-//    {
-//      lazy: true,
-//      animationEnabled: false,
-//      tabBarOptions: {
-//        showLabel: true,
-//          style: {
-//          backgroundColor: 'pink',
-//          height: 222,
-        
-//        }
-//      }
-//    }
-//  );
-
-// const App = createAppContainer(SimpleApp);
-// export default App;
-
-
+export default createAppContainer(screens);
